bf65e9cde6512be2904c9194d26853f6000f4dfa
Andy Carlson <andy@warp.dev>
2023-02-06 22:45:30 -0500

    remove unneeded examples

--- src/Applications/Application/Application.m
+++
@@ -1,8 +0,0 @@
-#include <Cocoa/Cocoa.h>
-
-int main(int argc, char* argv[]) {
-  NSWindow* window1 = [[[NSWindow alloc] initWithContentRect:NSMakeRect(100, 100, 300, 300) styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable backing:NSBackingStoreBuffered defer:NO] autorelease];
-  [window1 setIsVisible:YES];
-  [NSApplication sharedApplication];
-  [NSApp run];
-}
